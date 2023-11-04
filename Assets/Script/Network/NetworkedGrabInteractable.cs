273d7387f69dc9f81892dc403d4b452b9ef47310
Ali Leylekoğlu <ali.leylekoglu@ozu.edu.tr>
2023-11-05 02:10:39 +0300

    v4

---
+++ Assets/Script/Network/NetworkedGrabInteractable.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class NetworkedGrabInteractable : XRGrabInteractable, IPunObservable
+{
+    private PhotonView photonView;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        photonView = GetComponent<PhotonView>();
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        photonView.RequestOwnership();
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // We own this object: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            // Network player, receive data
+            transform.position = (Vector3)stream.ReceiveNext();
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
